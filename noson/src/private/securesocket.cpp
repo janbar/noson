cf15f18354a91accfcd155a6b9da4ca38c00e08a
janbar <jlbarriere68@gmail.com>
2026-03-02 17:11:33 +0100

    ssl: allow all ciphers except ones with no encryption

--- noson/src/private/securesocket.cpp
+++ noson/src/private/securesocket.cpp
@@ -60,7 +60,7 @@ void SSLSessionFactory::Destroy()
 #include <openssl/x509_vfy.h>
 
 /* Cipher suites, https://www.openssl.org/docs/apps/ciphers.html */
-const char* const PREFERRED_CIPHERS = "HIGH:!aNULL:!PSK:!SRP:!MD5:!RC4:!CAMELLIA:!DSS";
+const char* const PREFERRED_CIPHERS = "ALL:!aNULL";
 
 SSLSessionFactory::SSLSessionFactory()
 : m_client_ctx(nullptr)
